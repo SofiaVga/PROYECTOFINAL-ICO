5688bc5d17a01e5b949e9122ffc21c18effd41fe
SofiaVga <sofiavegagomez04@gmail.com>
2022-11-30 19:42:27 -0600

    Sexta version

--- ICO-PROYECTO.cpp
+++ ICO-PROYECTO.cpp
@@ -34,5 +34,29 @@ int main()
 			mes++;
 			printf("La fecha del dia siguiente es:%2d %2d %4d", dia, mes, anio);
 		}
+		else
+		{
+			printf("La fecha del dia siguiente es:%2d %2d %4d", dia, mes, anio);
+		}
+	}
+	else if (!((anio % 4 == 0 && anio % 100 != 0) || anio % 400 == 0) || mes != 2)
+	{
+		if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12)
+		{
+			if (dia == 32)
+			{
+				dia = 1;
+				mes++;
+			}
+			if (mes == 13)
+			{
+				mes = 01;
+				anio++;
+				printf("La fecha del dia siguiente es:%2d %2d %4d", dia, mes, anio);
+			}
+			else {
+				printf("La fecha del dia siguiente es:%2d %2d %4d", dia, mes, anio);
+			}
+		}
 	}
 }
